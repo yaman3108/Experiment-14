9a57cccef954d8fa97eedb1405078407bd2b1c88
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-09-09 16:02:27 +0530

    Update EXP14.3.cpp

--- EXP14.3.cpp
+++ EXP14.3.cpp
@@ -2,53 +2,34 @@
 #include <string>
 using namespace std;
 
-//Parent Class-1
+// Parent Class-1
 class Food {
-    public:
+public:
     string cuisine = "Indian";
-    void type(){
-        cout<<"Asian"<<endl;
+    
+    void type() {
+        cout << "Asian" << endl;
     }
 };
-//Child Class-1 (derived from parent-1)
-class Dish: public Food {
-    public:
+
+// Child Class-1 (derived from Parent-1)
+class Dish : public Food {
+public:
     string dish = "Biryani";
 };
-//Parent Class-2
-class Restaurant {
-    public:
+
+// Child Class-2 (derived from Child-1)
+class Restaurant : public Dish {
+public:
     string name = "Spice Kitchen";
-    void place() {
-        cout<<"India";
-    }
-};
-//Child Class-2 (derived from parent-1&2)
-class Combi: public Food, public Restaurant {
-    public:
-    string hotel = "JW Marriot";
-};
-//Child Class-3 (derived from child-1)
-class Classify: public Dish {
-    public:
-    string clas = "Malabar Biryani";
 };
 
-int main(){
-    //Single Inheritance
-    Dish f1;
-    f1.type();
-    cout<<f1.cuisine<<": "<<f1.dish<<endl;
+int main() {
+    Restaurant f3;
+    
+    f3.type();  
+    cout << f3.cuisine << ": " << f3.dish << endl;   
+    cout << "Restaurant: " << f3.name << endl;  
 
-    //Multiple Inheritance
-    Combi f2;
-    cout<<endl;
-    f2.place();
-    cout<<endl<<f2.cuisine<<": "<<f2.hotel<<endl<<f2.name<<endl;
-
-    //Multilevel Inheritance
-    Classify f3;
-    cout<<endl;
-    f3.type();
-    cout<<f3.cuisine<<": "<<f3.dish<<"("<<f3.clas<<")";
+    return 0;
 }
